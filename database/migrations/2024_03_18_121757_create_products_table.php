bd129279191fbbf046fda07e8fe406733d69cda5
smokeeaasd <lucasalencargomes111@gmail.com>
2024-03-21 12:40:45 -0300

    atualizando migrations

--- database/migrations/2024_03_18_121757_create_products_table.php
+++ database/migrations/2024_03_18_121757_create_products_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description');
+            $table->text('description');
             $table->decimal('price');
             $table->timestamps();
         });
